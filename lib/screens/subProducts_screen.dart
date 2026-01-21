c3ad760f199317ce63a4485be6f5ca187e6da9fb
MuhammadRizwan-dev <iamcoder231@gmail.com>
2026-01-21 18:45:21 +0500

    Fix import in subProducts screen
    Updated text

--- lib/screens/subProducts_screen.dart
+++ lib/screens/subProducts_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:grocery_app/screens/search_screen.dart';
 import 'package:grocery_app/screens/selectFilter_screen.dart';
-
 import '../components/utils.dart';
 
 class SubproductsScreen extends StatefulWidget {
